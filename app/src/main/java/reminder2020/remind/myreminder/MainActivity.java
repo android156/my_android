fb09775db0c0a3256be9e1862f59ad55265edfbe
android156 <n_degtyarev@mail.ru>
2021-06-17 01:36:41 +0300

    shit happens

---
+++ app/src/main/java/reminder2020/remind/myreminder/MainActivity.java
@@ -0,0 +1,15 @@
+package reminder2020.remind.myreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+    }
+}
